9a875eb1e4eb7fc67d1ad8eef94b060b4cd61ef7
HighX97 <loic.ortole@gmail.com>
2016-12-28 08:10:51 +0100

    HMIN306 TP2 AnalyseStatique

---
+++ src/tp2/step1/Parser.java
@@ -0,0 +1,87 @@
+package tp2.step1;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+
+import org.apache.commons.io.FileUtils;
+import org.eclipse.core.internal.utils.FileUtil;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+
+public class Parser {
+	
+	public static final String projectPath = "/home/lowx/Documents/Professionnel/Etude/Master_AIGLE/M2/workspace_eclipse/HMIN304_TP_1_2/";
+	public static final String projectSourcePath = projectPath + "/src";
+	//public static final String jrePath = "/usr/share/java";
+	public static final String jrePath = "";
+	
+	
+	
+	
+	public static void main(String[] args) throws IOException {
+
+		// read java files
+		final File folder = new File(projectSourcePath);
+		//Création de l'AST
+		ArrayList<File> javaFiles = listJavaFilesForFolder(folder);
+
+		//
+		for (File fileEntry : javaFiles) {
+			String content = FileUtils.readFileToString(fileEntry);
+			 System.out.println(content);
+
+			CompilationUnit parse = parse(content.toCharArray());
+
+		}
+	}
+
+	
+	
+	
+	// read all java files from specific folder
+	public static ArrayList<File> listJavaFilesForFolder(final File folder) {
+		ArrayList<File> javaFiles = new ArrayList<File>();
+		for (File fileEntry : folder.listFiles()) {
+			if (fileEntry.isDirectory()) {
+				javaFiles.addAll(listJavaFilesForFolder(fileEntry));
+			} else if (fileEntry.getName().contains(".java")) {
+				 System.out.println(fileEntry.getName());
+				javaFiles.add(fileEntry);
+			}
+		}
+
+		return javaFiles;
+	}
+	
+	//create AST
+	private static CompilationUnit parse(char[] classSource) {
+		ASTParser parser = ASTParser.newParser(AST.JLS4); // java +1.6
+		parser.setResolveBindings(true);
+		parser.setKind(ASTParser.K_COMPILATION_UNIT);
+ 
+		parser.setBindingsRecovery(true);
+ 
+		Map options = JavaCore.getOptions();
+		parser.setCompilerOptions(options);
+ 
+		parser.setUnitName("");
+ 
+		String[] sources = { projectSourcePath }; 
+		String[] classpath = {jrePath};
+ 
+		parser.setEnvironment(classpath, sources, new String[] { "UTF-8"}, true);
+		parser.setSource(classSource);
+		
+		return (CompilationUnit) parser.createAST(null); // create and parse
+	  }
+
+}
